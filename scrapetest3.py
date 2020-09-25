a603f979507330069e3e1189dd6ccd08a56589d2
DaLei Sun <sundalei1988@gmail.com>
2020-09-25 15:10:33 +0800

    test

--- scrapetest3.py
+++ scrapetest3.py
@@ -2,19 +2,20 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
 from bs4 import BeautifulSoup
+import re
 
 
 def get_title(url):
     try:
         html = urlopen(url)
-    except HTTPError as e:
+    except HTTPError:
         return None
-    except URLError as e:
+    except URLError:
         return None
     try:
         bs = BeautifulSoup(html.read(), 'html.parser')
         _title = bs.body.h1
-    except AttributeError as e:
+    except AttributeError:
         return None
     return _title
 
@@ -22,5 +23,10 @@ def get_title(url):
 html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html, 'html.parser')
 
-img = bs.find('img', {'src': '../img/gifts/img1.jpg'}).parent.previous_sibling.get_text()
-print(img)
+images = bs.find_all('img', {'src': re.compile(r'\.\.\/img\/gifts\/img.*\.jpg')})
+for image in images:
+    print(image['src'])
+
+result = bs.find_all(lambda tag: len(tag.attrs) == 2)
+for tag in result:
+    print(tag)
