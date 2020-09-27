7f2802f2f9475186ad1c727b8f48b10ff4e8ba9b
Leo Sun <leosun@Leos-MacBook-Pro.local>
2020-09-27 23:10:18 +0800

    test

---
+++ wiki-page.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import datetime
+import random
+import re
+
+random.seed(datetime.datetime.now())
+
+
+def get_links(article_url):
+    html = urlopen('http://en.wikipedia.org{}'.format(article_url))
+    bs = BeautifulSoup(html, 'html.parser')
+    return bs.find('div', {'id': 'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
+
+
+links = get_links('/wiki/Kevin_Bacon')
+while len(links) > 0:
+    new_article = links[random.randint(0, len(links) - 1)].attrs['href']
+    print(new_article)
+    links = get_links(new_article)
---
+++ wiki-page2.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+
+pages = set()
+
+def getLinks(pageUrl):
+    global pages
+    html = urlopen('http://en.wikipedia.org{}'.format(pageUrl))
+    bs = BeautifulSoup(html, 'html.parser')
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+        if link.attrs['href'] not in pages:
+            newPage = link.attrs['href']
+            print(newPage)
+            pages.add(newPage)
+            getLinks(newPage)
+
+getLinks('')
