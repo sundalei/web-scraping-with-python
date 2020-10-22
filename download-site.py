fc21bba2968cb6d90cc4c1769de3b2aa0f8a6f6c
Leo Sun <leosun@Leos-MacBook-Pro.local>
2020-10-22 23:17:48 +0800

    test

--- download-site.py
+++ download-site.py
@@ -2,7 +2,17 @@ from urllib.request import urlretrieve
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+base_url = 'http://pythonscraping.com'
+
+
+def get_absolute_url(base_url, source):
+    print(base_url)
+    print(source)
+    return source
+
 html = urlopen('http://www.pythonscraping.com/')
 bs = BeautifulSoup(html, 'html.parser')
-image_location = bs.find('a', {'id': 'logo'}).find('img')['src']
-urlretrieve(image_location, 'logo.jpg')
+download_list = bs.find_all(src=True)
+
+for download in download_list:
+    file_url = get_absolute_url(base_url, download['src'])
