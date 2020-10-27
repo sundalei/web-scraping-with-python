c5061f2c7ffc3fda7c38cb24e64cb8b380ca9f95
DaLei Sun <sundalei1988@gmail.com>
2020-10-27 11:00:08 +0800

    ''test

--- download_mysql.py
+++ download_mysql.py
@@ -1,10 +1,11 @@
 from urllib.request import urlopen
+from urllib.request import Request
 from bs4 import BeautifulSoup
 import datetime
 import random
 import pymysql
 
-conn = pymysql.connect(host='127.0.0.1', user='root', passwd='19880701', db='mysql', charset='utf8')
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='Fnst*1234', db='mysql', charset='utf8')
 cur = conn.cursor()
 cur.execute('USE scraping')
 
@@ -12,21 +13,37 @@ random.seed(datetime.datetime.now())
 
 
 def store(title, content):
-    cur.execute('INSERT INTO pages (title, content) VALUES ("%s", "%s")', (title, content))
+    cur.execute('INSERT INTO pages (title, content) VALUES (%s, %s)', (title, content))
     cur.connection.commit()
 
 
 def get_links(article_url):
-    html = urlopen('http://en.wikipedia.org' + article_url)
+    req = Request(article_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'})
+    html = urlopen(req)
+    bs = BeautifulSoup(html, 'html.parser')
+    articles = bs.find_all('article', {'class': 'post'})
+    links = [article.find('a')['href'] for article in articles]
+    return links
+
+
+def get_article(link):
+    req = Request(link, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'})
+    html = urlopen(req)
     bs = BeautifulSoup(html, 'html.parser')
     title = bs.find('h1').get_text()
-    content = bs.find('div', {'id': 'mw-content-text'}).find_all('p').get_text()
-    print(content)
+    content_section = bs.find('h4')
+    if content_section == None:
+        content = title
+    else:
+        content = content_section.get_text()
+    
+    print(title, content)
+    store(title, content)
 
-
-links = get_links('/wiki/Kevin_Bacon')
-
-# cur.execute('SELECT * FROM pages')
-# print(cur.fetchall())
-# cur.close()
-# conn.close()
+links = get_links('http://www.allitebooks.com/')
+try:
+    for link in links:
+        get_article(link)
+finally:
+    cur.close()
+    conn.close()
