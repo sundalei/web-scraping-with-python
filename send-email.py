2c8f465e5154d7d8ffe76e2c089d0c1acc6b9b38
DaLei Sun <sundalei1988@gmail.com>
2020-10-30 12:13:39 +0800

    'test'

--- send-email.py
+++ send-email.py
@@ -1,5 +1,8 @@
 import smtplib
 from email.mime.text import MIMEText
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import time
 
 mail_host = 'smtp.163.com'
 mail_user = 'sundalei2011@163.com'
@@ -8,14 +11,22 @@ mail_password = 'YTKPXXRKISOTASSV'
 sender = 'sundalei2011@163.com'
 receivers = ['sundalei1988@gmail.com']
 
-msg = MIMEText('The body of the email is here')
+def send_mail(subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = '{}'.format(sender)
+    msg['To'] = ','.join(receivers)
+    
+    s = smtplib.SMTP('')
+    s.connect(mail_host, 25)
+    s.login(mail_user, mail_password)
+    s.send_message(msg)
+    s.quit()
 
-msg['Subject'] = 'An Email Alert'
-msg['From'] = '{}'.format(sender)
-msg['To'] = ','.join(receivers)
+bs = BeautifulSoup(urlopen('https://isitchristmas.com/'), 'html.parser')
+while(bs.find('a', {'id':'answer'}).attrs['title'] == 'NO'):
+    print('It is not Christmas yet.')
+    time.sleep(3600)
+    bs = BeautifulSoup(urlopen('https://isitchristmas.com/'), 'html.parser')
 
-s = smtplib.SMTP('')
-s.connect(mail_host, 25)
-s.login(mail_user, mail_password)
-s.send_message(msg)
-s.quit()
+send_mail('It\'s Christmas!', 'According to http://itischristmas.com, it is Christmas!')
