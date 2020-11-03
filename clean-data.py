f794c171be9d71e0c3144e80d3475011d67a6ee9
Leo Sun <leosun@Leos-MacBook-Pro.local>
2020-11-03 23:01:35 +0800

    test

--- clean-data.py
+++ clean-data.py
@@ -1,22 +1,38 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import string
+
+def cleanSentence(sentence):
+    sentence = sentence.split(' ')
+    sentence = [word.strip(string.punctuation + string.whitespace) for word in sentence]
+    sentence = [word for word in sentence if len(word) > 1 or (word.lower() == 'a' or word.lower() == 'i')]
+    return sentence
 
 def cleanInput(content):
     content = re.sub('\n|[\[\d+\]]', ' ', content)
     content = bytes(content, 'utf-8')
     content = content.decode('ascii', 'ignore')
-    return content
+    sentences = content.split('. ')
+    return [cleanSentence(sentence) for sentence in sentences]
 
+def getNgramsFromSentence(content, n):
+    output = []
+    for i in range(len(content)-n+1):
+        output.append(content[i:i+n])
+    return output
 
 def getNgrams(content, n):
     content = cleanInput(content)
-    print(content)
+    ngrams = []
+    for sentence in content:
+        ngrams.extend(getNgramsFromSentence(sentence, n))
+    return ngrams
 
-# html = urlopen('https://en.wikipedia.org/wiki/Python_(programming_language)')
-# bs = BeautifulSoup(html, 'html.parser')
-# content = bs.find('div', {'id': 'mw-content-text'}).get_text()
-content = 'Python features a dynamic type syst[123]em and automatic memory management. It supports multiple programming paradigms...'
+html = urlopen('https://en.wikipedia.org/wiki/Python_(programming_language)')
+bs = BeautifulSoup(html, 'html.parser')
+content = bs.find('div', {'id': 'mw-content-text'}).get_text()
+# content = 'Python features a dynamic type syst[123]em and automatic memory management. It supports multiple programming paradigms...'
 ngrams = getNgrams(content, 2)
-# print(ngrams)
-# print('2-grams count is: ' + str(len(ngrams)))
+print(ngrams)
+print('2-grams count is: ' + str(len(ngrams)))
