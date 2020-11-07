8c3d663c15f47539c6e92cd191f4e7a6ad77f1a0
Leo Sun <leosun@Leos-MacBook-Pro.local>
2020-11-07 21:37:45 +0800

    test

--- markov-text.py
+++ markov-text.py
@@ -2,10 +2,17 @@ from urllib.request import urlopen
 from random import randint
 
 def wordListSum(wordList):
-    pass
+    sum = 0
+    for word, value in wordList.items():
+        sum += value
+    return sum
 
 def retrieveRandomWord(wordList):
     randIndex = randint(1, wordListSum(wordList))
+    for word, value in wordList.items():
+        randIndex -= value
+        if randIndex <= 0:
+            return word
 
 def buildWordDict(text):
     # Remove newlines and quotes
