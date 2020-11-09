bc6303840f1e2bf60e8ebc343cb902d94d24e6e1
DaLei Sun <sundalei1988@gmail.com>
2020-11-09 17:40:09 +0800

    'test'

--- markov-wikipedia.py
+++ markov-wikipedia.py
@@ -4,11 +4,32 @@ conn = pymysql.connect(host='127.0.0.1', user='root', passwd='Fnst*1234', db='my
 cur = conn.cursor()
 cur.execute('USE wikipedia')
 
+def getUrl(pageId):
+    cur.execute('SELECT url FROM pages WHERE id = %s', (int(pageId)))
+    return cur.fetchone()[0]
+
 def getLinks(fromPageId):
-    pass
+    cur.execute('select toPageId from links where fromPageId = %s', (int(fromPageId)))
+    if cur.rowcount == 0:
+        return []
+    return [x[0] for x in cur.fetchall()]
+
+def searchBreadth(targetPageId, paths=[[1]]):
+    newPaths = []
+    for path in paths:
+        links = getLinks(path[-1])
+        for link in links:
+            if link == targetPageId:
+                return path + [link]
+            else:
+                newPaths.append(path + [link])
+    return searchBreadth(targetPageId, newPaths)
 
 nodes = getLinks(1)
-
+targetPageId = 31035
+pageIds = searchBreadth(targetPageId)
+for pageId in pageIds:
+    print(getUrl(pageId))
 
 cur.close()
 conn.close()
