9c51bc24240fc28054e8558900b614463b45e51f
Leo Sun <leosun@Leos-MacBook-Pro.local>
2020-11-10 23:56:28 +0800

    test

--- nltk-text.py
+++ nltk-text.py
@@ -1,11 +1,19 @@
 from nltk.book import *
 from nltk import FreqDist
+from nltk import bigrams
+from nltk import ngrams
 
-print(text6)
-print(len(text6))
-print(len(set(text6)))
 print(len(text6)/len(set(text6)))
 
 fdist = FreqDist(text6)
 result = fdist.most_common(20)
 print(result)
+
+bigrams = bigrams(text6)
+bigramsDist = FreqDist(bigrams)
+print(bigramsDist[('Sir', 'Robin')])
+
+fourgrams = ngrams(text6, 4)
+for fourgram in fourgrams:
+    if fourgram[0] == 'coconut':
+        print(fourgram)
