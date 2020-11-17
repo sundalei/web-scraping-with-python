da7dd7702436770c2f172cdaa77af69358d4b790
DaLei Sun <sundalei1988@gmail.com>
2020-11-17 15:59:50 +0800

    'test'

--- selenium-demo2.py
+++ selenium-demo2.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.PhantomJS()
 driver.get('http://pythonscraping.com/pages/javascript/ajaxDemo.html')
 try:
-    pass
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(By.ID, 'loadedButton'))
 finally:
-    pass
+    print(driver.find_element_by_id('content').text)
+    driver.close()
